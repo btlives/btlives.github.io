e28b36e4c15df4f0ec2635210f7d503b1a4cf863
BTL (Blades Take Lives) <45032632+btlives@users.noreply.github.com>
2018-12-06 00:17:39 +0000

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -3,3 +3,22 @@
    $visitor_email = $_POST['email'];
    $message = $_POST['message'];
    
+
+   $email_from = 'btlives1@gmail.com';
+   
+   $email_subject = "New Form Submission";
+
+   $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                       "User Message: $message.\n";
+
+
+$to = "btlives1@gmail.com";
+
+$headers = "From" $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
